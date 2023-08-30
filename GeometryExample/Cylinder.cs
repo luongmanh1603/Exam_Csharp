b501c7b0c845099def669dad5419dcd27a8ae28c
luongmanh1604 <114908089+luongmanh1604@users.noreply.github.com>
2023-08-30 14:42:02 +0700

    Add project files.

---
+++ GeometryExample/Cylinder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryExample
+{
+    internal class Cylinder
+    {
+        private double Radius {  get; set; }
+        private double Height { get; set;}
+        public void Process()
+        {
+            Console.WriteLine("Nhap ban kinh cua xi lanh: ");
+            Radius = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Nhập chiều cao của xi lanh: ");
+            Height = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public void Result()
+        {
+            double BaseArea = Radius * Radius * Math.PI;
+            double LateralArea = 2 * Math.PI * Radius * Height;
+            double TotalArea = 2 * Math.PI * Radius * (Height + Radius);
+            double Volume = Math.PI * Radius * Radius * Height;
+
+            Console.WriteLine("Kết quả tính toán:");
+            Console.WriteLine($"Diện tích đáy: {BaseArea}");
+            Console.WriteLine($"Diện tích bề mặt xung quanh: {LateralArea}");
+            Console.WriteLine($"Tổng diện tích bề mặt: {TotalArea}");
+            Console.WriteLine($"Thể tích: {Volume}");
+        }
+    }
+}
---
+++ GeometryExample/Program.cs
@@ -0,0 +1,15 @@
+﻿using GeometryExample;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Cylinder cylinder = new Cylinder();
+
+        Console.WriteLine("Ứng dụng tính diện tích và thể tích của xi lanh");
+        Console.WriteLine("---------------------------------------------");
+
+        cylinder.Process();
+        cylinder.Result();
+    }
+}
